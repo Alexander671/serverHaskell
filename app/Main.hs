49e5c923fe055997f4a90f1f5c0e7fdfdd80df9f
Alexander671 <matveevalex97@yandex.ru>
2020-08-07 00:35:14 +0300

    Проблема при сопастовлении с образцом jwt поля userRole

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,18 @@ module Main where
 {----------------------------------------}
 import Lib
 import DB
+--import JWT
+import Web.JWT as JWT (ClaimsMap, unregisteredClaims, 
+                       claims, header, unClaimsMap, signature,
+                       Signer(HMACSecret), decode,decodeAndVerifySignature) 
 {----------------------------------------}
-import Prelude                  hiding (lookup)
+import Prelude
 {----------------------------------------}
 import qualified Data.ByteString.Char8    as BS
 import           Data.ByteString.Lazy     (ByteString)
 import qualified Data.ByteString.Lazy     as LBS
 {----------------------------------------}
-import           Data.Map.Strict          (Map, fromList, keys, lookup)
+import           Data.Map.Strict       (toList, Map, fromList, keys, lookup) 
 import           Data.Text                as DT 
 import Network.URI.Encode (decodeBSToText,encodeTextToBS)      
 {----------------------------------------}
@@ -25,7 +29,10 @@ import           Network.Wai              (Application, ResponseReceived, Reques
                                            responseLBS, responseStatus)
 import Network.Wai.Handler.Warp (run)
 {----------------------------------------}
-import Data.Aeson
+import Data.Aeson as DA
+import Data.Time (Day)
+import           GHC.Generics  
+
 
 
 type PathName        = BS.ByteString
@@ -34,7 +41,6 @@ type PathArg         = BS.ByteString
 type PathResult      = BS.ByteString
 type PathSpec        = ( PathDescription
                            , (PathArg -> PathResult) )
-
 
 -- Main function
 
@@ -45,28 +51,59 @@ main = do
 
 -- web app
 application :: Application
-application req respond
+application req respond =              
+      case (fmap (Prelude.lookup "userRole") $ jwtcheck route) of
+       Just (Just role) -> application2 role req respond
+       Just (Nothing)   -> respond $ responseBadRequestJSON "error: 1"
+       Nothing          -> respond $ responseBadRequestJSON "error: 2"
+      ----------------------------
+      where route = unpack $ Prelude.head $ head1 (pathInfo req)
+            jwtcheck content = fmap (toList . unClaimsMap . unregisteredClaims . JWT.claims) (JWT.decodeAndVerifySignature (HMACSecret "your secret") $ DT.pack content)
+
+head1 [] = ["error: empty list"]
+head1 (x:xs) = [x]
+
+
+application2 :: Value -> Application
+application2 role req respond
   | requestMethod req == methodGet =
-    case pathInfo req of
-     ["news"]     -> case queryString req of
-                    [] -> routeNews "SELECT * FROM news;" req respond
-                    [("category", Nothing)]  -> routeNews "SELECT * FROM news ORDER BY category ASC;" req respond
-                    [("date_of_create_at",Just x)] -> routeNews (BS.pack $ "SELECT * FROM news WHERE date_of_create= '" ++ show x ++ "';") req respond
-                    _  -> respond $ responseNotFoundJSON $ encode ("error" ::Text)
-
-     ["autors"]   -> routeAutors   req respond
-     ["users"]    -> routeUsers    req respond
-     ["comments"] -> routeComments req respond
-     _            -> respond $ responseOkJSON $ encode ("error"::Text)
+  case pathInfo req of
+    (_:"news":_)     -> routeNews     req respond   
+    (_:"autors":_)   -> routeAutors   req respond
+    (_:"users":_)    -> routeUsers    req respond
+    (_:"comments":_) -> routeComments req respond
+    _              -> respond $ responseOkJSON $ encode ("error"::Text)  
   | otherwise =
-    respond
-    $ responseBadRequestJSON "Only GET method is allowed!"
+    respond $ responseBadRequestJSON "Only GET method is allowed!"
 
--- Get method (news,autors,users)  
-routeNews :: BS.ByteString ->  Application
-routeNews sql req respond = do
-    res <- connectToDB sql someNewsDecoder
-    (respond $ responseOkJSON $ encode res)
+routeNews :: Application
+routeNews req respond = 
+    case pathInfo req of
+    [_,"news"] -> dbNews "SELECT * FROM news;" req respond
+    [_,"news","filter"] ->
+        case queryString req of 
+        _:("date_of_create_at_gt",Just x):xs -> dbNews (BS.pack $ "SELECT * FROM news WHERE date_of_create > '" ++ show x ++ "';") req respond
+        _:("date_of_create_at_lt",Just x):xs -> dbNews (BS.pack $ "SELECT * FROM news WHERE date_of_create < '" ++ show x ++ "';") req respond
+        _:("name_of_autor", Just x):xs       -> dbNews (BS.pack $ "SELECT n.*, u.second_name, u.first_name FROM news n, users u WHERE n.autor_id = u.user_id  AND u.first_name = '" ++ show x ++ "';") req respond
+        _:("category", Just x):xs            -> dbNews (BS.pack $ "SELECT * FROM news WHERE category = '" ++ BS.unpack x ++ "';") req respond
+        -- name_of_autor, изменить структуру бд, а также типы в Types, сделать поле user... ::Users 
+        _  -> respond $ responseNotFoundJSON $ encode ("error" ::Text)
+    [_,"news","order"] ->
+        case queryString req of
+        [] -> respond $ responseNotFoundJSON $ encode ("error" ::Text)
+        _:("date_of_create",Just x):xs  -> dbNews (BS.pack $ "SELECT * FROM news ORDER BY date_of_create " ++ show x ++ "';") req respond
+        _:("category",Just x):xs        -> dbNews (BS.pack $ "SELECT * FROM news ORDER BY category " ++ BS.unpack x ++ ";") req respond
+        _:("name_of_autor", Just x):xs  -> dbNews (BS.pack $ "SELECT n.*, u.second_name, u.first_name FROM news n, users u WHERE n.autor_id = u.user_id  ORDER BY u.first_name " ++ BS.unpack x ++ ";") req respond
+        _ -> respond $ responseNotFoundJSON $ encode ("error" ::Text)
+        {------------------------------------------------------------}
+
+dbNews :: BS.ByteString ->  Application
+dbNews sql req respond = do 
+  res <- connectToDB sql someAutorsDecoder
+  (respond $ responseOkJSON
+              (case rawQueryString req of
+                "" -> encode res
+                _  -> encode ("error"::Text))) 
               
 
 routeAutors :: Application
@@ -92,7 +129,6 @@ routeComments req respond = do
               (case rawQueryString req of
                 "" -> encode res
                 _  -> encode ("Not available path"::Text))) 
-
 
 responseOkJSON,responseNotFoundJSON,responseBadRequestJSON :: ByteString -> Response
 responseOkJSON = responsePlainTextJSON status200
